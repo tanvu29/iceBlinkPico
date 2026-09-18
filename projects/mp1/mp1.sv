130d35a2a3a6227e36b9237d529570d11bbc1938
tanvu29 <tanvu.ocs@gmail.com>
2026-09-18 10:09:11 -0400

    Submit mini project 1

---
+++ projects/mp1/mp1.sv
@@ -0,0 +1,86 @@
+// RGB cycle
+
+module mp1(
+    input logic     clk,
+    output logic    RGB_R,
+    output logic    RGB_G,
+    output logic    RGB_B
+);
+
+    // CLK frequency is 12MHz, so 2,000,000 CLK cycles is 1/6 of a second
+    parameter COLOR_INTERVAL = 2000000;
+
+    // LED timer
+    logic [$clog2(COLOR_INTERVAL) - 1:0] count = 0;
+
+    // stores the 6 required RGB states
+    logic [2:0] index = 0;
+
+    initial begin
+        RGB_R = 1'b0;
+        RGB_G = 1'b0;
+        RGB_B = 1'b0;
+    end
+
+    always_ff @(posedge clk) begin
+        if (count == COLOR_INTERVAL - 1) begin
+            count <= 0;
+            index <= (index == 5) ? 0 : index + 1;
+        end
+
+        else begin
+            count <= count + 1;
+        end
+    end
+
+    always_comb begin
+        case (index)
+            // RED
+            3'b000 : begin
+                RGB_R = 1'b1;
+                RGB_G = 1'b0;
+                RGB_B = 1'b0;
+            end
+            // YELLOW
+            3'b001 : begin
+                RGB_R = 1'b1;
+                RGB_G = 1'b1;
+                RGB_B = 1'b0;
+            end
+            // GREEN
+            3'b010 : begin
+                RGB_R = 1'b0;
+                RGB_G = 1'b1;
+                RGB_B = 1'b0;
+            end
+            // CYAN
+            3'b011 : begin
+                RGB_R = 1'b0;
+                RGB_G = 1'b1;
+                RGB_B = 1'b1;
+            end
+            // BLUE
+            3'b100 : begin
+                RGB_R = 1'b0;
+                RGB_G = 1'b0;
+                RGB_B = 1'b1;
+            end
+            // MAGENTA
+            3'b101 : begin
+                RGB_R = 1'b1;
+                RGB_G = 1'b0;
+                RGB_B = 1'b1;
+            end
+            // default: all off
+            default: begin
+                RGB_R = 1'b0;
+                RGB_G = 1'b0;
+                RGB_B = 1'b0;
+            end
+        endcase
+    end
+
+
+
+
+endmodule
